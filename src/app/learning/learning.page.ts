4fbf3666d686dcdb7f99abba8c8cac04be030e40
clebercmsf <cleber.cmsf@gmail.com>
2024-04-04 15:57:23 -0300

    learn-page

--- src/app/learning/learning.page.ts
+++ src/app/learning/learning.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-learning',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LearningPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  moveToHome () {
+    this.router.navigate(['/home']);
+  }
 }
